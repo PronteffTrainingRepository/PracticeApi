754b584aaa15e445c91e83a3920574e2f915bb01
shubhampronteff <67738104+shubhampronteff@users.noreply.github.com>
2020-08-27 19:24:07 +0530

    27/08/2020

--- App.js
+++ App.js
@@ -12,44 +12,44 @@ import PokeData from "./src/PokeData";
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <GetCla />
-      <GetFun /> */}
-    </View>
-    // <NavigationContainer>
-    //   <Stack.Navigator initialRouteName="pokemon">
-    //     <Stack.Screen
-    //       name="pokemon"
-    //       component={Pokemon}
-    //       options={{
-    //         title: "List Of PokeMons",
-    //         headerTitleAlign: "center",
-    //         headerStyle: {
-    //           backgroundColor: "#f4511e",
-    //         },
-    //         headerTintColor: "#fff",
-    //         headerTitleStyle: {
-    //           fontWeight: "bold",
-    //         },
-    //       }}
-    //     />
-    //     <Stack.Screen
-    //       name="pokedata"
-    //       component={PokeData}
-    //       options={{
-    //         title: "Pokemon's Information",
-    //         headerTitleAlign: "center",
-    //         headerStyle: {
-    //           backgroundColor: "#f4511e",
-    //         },
-    //         headerTintColor: "#fff",
-    //         headerTitleStyle: {
-    //           fontWeight: "bold",
-    //         },
-    //       }}
-    //     />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    // <View style={styles.container}>
+    //   {/* <GetCla />
+    //   <GetFun /> */}
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="pokemon">
+        <Stack.Screen
+          name="pokemon"
+          component={Pokemon}
+          options={{
+            title: "List Of PokeMons",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="pokedata"
+          component={PokeData}
+          options={{
+            title: "Pokemon's Information",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
